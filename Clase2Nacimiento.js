887798147174bb28a9e69d735d8f92e3b47b56e6
ppablomn <90877741+ppablomn@users.noreply.github.com>
2021-09-21 13:24:23 -0300

    Add files via upload

---
+++ Clase2Nacimiento.js
@@ -0,0 +1,13 @@
+let calificacion = Number(prompt("ingrese su calificacion por favor"));
+
+if(calificacion >= 1 && calificacion <= 10){
+if (calificacion < 7) {
+    alert("HAS REPROBADO");
+}
+else if (calificacion => 7) {
+    alert("HAS APROBADO");
+}
+}
+else {
+    alert("DATO INCORRECTO. INGRESAR UN VALOR ENTRE 1 Y 10.");
+}
